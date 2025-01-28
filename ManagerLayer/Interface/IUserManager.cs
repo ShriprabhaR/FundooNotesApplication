68fff3cc38d3bb5518fe0ab738999def6644e110
ShriprabhaR <raichurprabha1@gmail.com>
2025-01-28 15:40:10 +0530

    [Shriprabha]added: Business logic layer

---
+++ ManagerLayer/Interface/IUserManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using RepositoryLayer.Entity;
+
+namespace ManagerLayer.Interface
+{
+    public interface IUserManager
+    {
+        public Users Registration(RegisterModel model);
+        public string Login(LoginModel login);
+
+        public bool TestEmail(string email);
+    }
+}
---
+++ ManagerLayer/Services/UserManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonLayer.Models;
+using ManagerLayer.Interface;
+using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+
+namespace ManagerLayer.Services
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IUserRepository user;
+        private readonly FunDooDBContext context;
+
+        public UserManager(IUserRepository user, FunDooDBContext context)
+        {
+            this.user = user;
+            this.context = context;
+        }
+
+        public Users Registration(RegisterModel model)
+        {
+            return user.Registration(model);
+        }
+
+        public string Login(LoginModel login)
+        {
+            return user.Login(login);
+        }
+
+        public bool TestEmail(string email)
+        {
+            var checkMail = this.context.Users.FirstOrDefault(x => x.Email == email);
+
+            if (checkMail!=null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
