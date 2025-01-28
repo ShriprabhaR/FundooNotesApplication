39bc6618d3937955c91fcdb2537730c8991ed4fe
ShriprabhaR <raichurprabha1@gmail.com>
2025-01-28 15:22:56 +0530

    [Shriprabha]added: Presentation layer

---
+++ FundooNotesApp/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using CommonLayer.Models;
+using ManagerLayer.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.Entity;
+
+namespace FundooNotesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserManager manager;
+
+        public UsersController(IUserManager manager)
+        {
+            this.manager = manager;
+        }
+        [HttpPost]
+        [Route("Reg")]
+
+        public IActionResult Register(RegisterModel model)
+        {
+            var checkMail = manager.TestEmail(model.Email);
+            if (checkMail)
+            {
+                return BadRequest(new ResponseModel<bool> { Success = true, Message = "Mail already exist", Data = checkMail });
+            }
+            else
+            {
+                var result = manager.Registration(model);
+                if (result != null)
+                {
+                    return Ok(new ResponseModel<Users> { Success = true, Message = "Register Successful", Data = result });
+                }
+                else
+                {
+                    return BadRequest(new ResponseModel<Users> { Success = false, Message = "Register failed" });
+                }
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult UserLogin(LoginModel model)
+        {
+            var response = manager.Login(model);
+            if (response != null)
+            {
+                return Ok(new ResponseModel<string> { Success = true,Message="Logged in successfully", Data= response });
+            }
+            return BadRequest(new ResponseModel<string> { Success= false, Message="login failed", Data=response });
+        }
+           
+    }
+}
---
+++ FundooNotesApp/Startup.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ManagerLayer.Interface;
+using ManagerLayer.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RepositoryLayer.Context;
+using RepositoryLayer.Interface;
+using RepositoryLayer.Services;
+
+namespace FundooNotesApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<FunDooDBContext>(a => a.UseSqlServer(Configuration["ConnectionStrings:DBConnection"]));
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IUserManager, UserManager>();
+            services.AddSwaggerGen();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fundoo API V1");
+            });
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
