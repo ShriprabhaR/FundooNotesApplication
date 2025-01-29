ba90598254b05b13c47465522e6c3a35a17e4b70
ShriprabhaR <raichurprabha1@gmail.com>
2025-01-29 17:38:22 +0530

    [Shriprabha] added:Send Email method

---
+++ CommonLayer/Models/Send.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class Send
+    {
+        public string SendMail(string ToEmail, string Token)
+        {
+            string FromEmail = "raichurprabha1@gmail.com";
+            MailMessage message = new MailMessage(FromEmail, ToEmail);
+            string MailBody = "Token for the reset password: " + Token;
+            message.Subject = "Token generated for resetting password";
+            message.Body = MailBody.ToString();
+            message.BodyEncoding = Encoding.UTF8;
+            message.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            NetworkCredential credential = new NetworkCredential("raichurprabha1@gmail.com", "wdig abpw ztbh xmmt");
+
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false; //not using default credential
+            smtpClient.Credentials = credential;  //
+
+            smtpClient.Send(message);
+            return ToEmail;
+
+        }
+    }
+}
