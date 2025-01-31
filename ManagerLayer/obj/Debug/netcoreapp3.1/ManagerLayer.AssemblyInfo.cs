79d6ffe2a954402abab912535dc61b211524e1b8
ShriprabhaR <raichurprabha1@gmail.com>
2025-01-31 17:42:40 +0530

    [Shriprabha] Modified files

--- ManagerLayer/obj/Debug/netcoreapp3.1/ManagerLayer.AssemblyInfo.cs
+++ ManagerLayer/obj/Debug/netcoreapp3.1/ManagerLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagerLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eea24470b1a64fcbe60deefb5c7d08031aff195")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c4a041673d429e360fdef9ff4e5ac33146d898")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagerLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagerLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
