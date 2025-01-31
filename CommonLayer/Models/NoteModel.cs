5d3585f90fed872672fed8156ee9f3342d08c95e
ShriprabhaR <raichurprabha1@gmail.com>
2025-01-31 17:28:17 +0530

    [Shriprabha] Model for Add Notes

---
+++ CommonLayer/Models/NoteModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class NoteModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
