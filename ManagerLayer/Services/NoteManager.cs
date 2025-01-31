a059863ea1313d2ecf97cd5569cc8e9049383852
ShriprabhaR <raichurprabha1@gmail.com>
2025-01-31 17:31:29 +0530

    [Shriprabha] Manager Layer for notes

---
+++ ManagerLayer/Services/NoteManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using ManagerLayer.Interface;
+using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+
+namespace ManagerLayer.Services
+{
+    public class NoteManager : INoteManager
+    {
+        private readonly INotesRepository note;
+        private readonly FunDooDBContext context;
+
+        public NoteManager(INotesRepository note, FunDooDBContext context)
+        {
+            this.note = note;
+            this.context = context;
+        }
+
+        public Notes AddNote(int UserId, NoteModel model)
+        {
+            return note.AddNote(UserId, model);
+        }
+
+        public List<Notes> GetNotes(int UserId)
+        {
+            return note.GetNotes(UserId);
+        }
+
+        public Notes UpdateNotes(int UserId, int NotesId, UpdateNotesModel model)
+        {
+            return note.UpdateNotes( UserId, NotesId, model);
+        }
+
+        public bool DeleteNotes(int NotesId, int UserId)
+        {
+            return note.DeleteNotes( NotesId, UserId);
+        }
+
+    }
+}
