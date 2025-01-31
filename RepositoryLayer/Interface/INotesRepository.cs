cf63d46ef6bd7310ea047a5bb56fff3e5bae3d94
ShriprabhaR <raichurprabha1@gmail.com>
2025-01-31 17:33:21 +0530

    [Shriprabha] Repository layer Interface for Notes

---
+++ RepositoryLayer/Interface/INotesRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using RepositoryLayer.Entity;
+
+namespace RepositoryLayer.Interface
+{
+    public interface INotesRepository
+    {
+        public Notes AddNote(int UserId, NoteModel model);
+
+        public List<Notes> GetNotes(int UserId);
+
+        public Notes UpdateNotes(int UserId, int NotesId, UpdateNotesModel model);
+
+        public bool DeleteNotes(int NotesId, int UserId);
+    }
+}
