edfafe476fb8fa610c447b611b1fe574f779f3c5
ShriprabhaR <raichurprabha1@gmail.com>
2025-01-31 17:28:58 +0530

    [Shriprabha] Model for Update Notes

---
+++ CommonLayer/Models/UpdateNotesModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class UpdateNotesModel
+    {
+        public int NotesId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Reminder { get; set; }
+        public string Color { get; set; }
+        public string Image { get; set; }
+        public bool IsArchive { get; set; }
+        public bool IsPin { get; set; }
+        public bool IsTrash { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+
+
+    }
+}
