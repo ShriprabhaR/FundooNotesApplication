9bfc43a1b42f7aacc93912956b096a7fddefcad4
ShriprabhaR <raichurprabha1@gmail.com>
2025-02-01 12:50:40 +0530

    [shriprabha]added Modified files

--- CommonLayer/obj/Debug/netcoreapp3.1/CommonLayer.AssemblyInfo.cs
+++ CommonLayer/obj/Debug/netcoreapp3.1/CommonLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+301ff000be6c11444b982a7c5399e3dd803c336c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd5812ce06efd76acc806f9561e95010bca2fe54")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ManagerLayer/obj/Debug/netcoreapp3.1/ManagerLayer.AssemblyInfo.cs
+++ ManagerLayer/obj/Debug/netcoreapp3.1/ManagerLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagerLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+301ff000be6c11444b982a7c5399e3dd803c336c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd5812ce06efd76acc806f9561e95010bca2fe54")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagerLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagerLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
