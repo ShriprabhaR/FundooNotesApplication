bef787c3356b7770bce09ce3b90901b304cc1076
ShriprabhaR <121273908+ShriprabhaR@users.noreply.github.com>
2025-02-05 14:06:03 +0530

    Add files via upload
    Helper of Fundoo App

---
+++ Helper/AppException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FundooNotesApp.Helper
+{
+    public class AppException : Exception
+    {
+
+        // Custom exception class for throwing application specific exceptions 
+        // that can be caught and handled within the application
+
+        public AppException() : base() { }
+
+        public AppException(string message) : base(message) { }
+
+    }
+}
